dfeee5257ed26ba1b5800446be441a90358b2cc7
Harbans <harbansbhandal@gmail.com>
2018-01-13 07:21:58 -0800

    its wonky

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'EjKxsCOLavkKCq0qR0eLbhoZa',
+  consumer_secret: 'tRkxGkfccdTR2yxP0sREEINtsIEqZg75dCS8EWO4BLb3VXOCRs',
+  access_token_key: '918615589752418304-C2ED1LbT2WOxLaSIpDISF6AYT6KRFDi',
+  access_token_secret: 'RKwFMIBPiSEzO42b79Mxn4tXgJqTyV51PKPikzgWg3IGY',
+}
+
+exports.tmdbKey = '8d5f41f6535c312ee8ea248cecd733f7';
